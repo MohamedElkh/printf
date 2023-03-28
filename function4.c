fb3db39b04d21b8b7915777c8815ec0e841e3c09
MohamedElKh <mohamedkharashy27@yahoo.com>
2023-03-28 02:54:56 -0700

    0

--- function4.c
+++ function4.c
@@ -9,15 +9,15 @@
 
 int get_flags(const char *format, int *i)
 {
-	int x, curr_i;
+	int x, curr;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	for (curr = *i + 1; format[curr] != '\0'; curr++)
 	{
 		for (x = 0; FLAGS_CH[x] != '\0'; x++)
-			if (format[curr_i] == FLAGS_CH[x])
+			if (format[curr] == FLAGS_CH[x])
 			{
 				flags |= FLAGS_ARR[x];
 				break;
@@ -26,7 +26,7 @@ int get_flags(const char *format, int *i)
 		if (FLAGS_CH[x] == 0)
 			break;
 	}
-	*i = curr_i - 1;
+	*i = curr - 1;
 	return (flags);
 }
 
