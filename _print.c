d4e9b58b27306a5e4cbc920c0d2cc9c83b4f3fca
Caryordey <117972535+Caryordey@users.noreply.github.com>
2023-03-29 02:12:04 +0100

    Update _print.c

--- _print.c
+++ _print.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
- * _print - writes the character c to stdout
+ * _print - writes the character c to stdout with
  * @c: The character to print
  *
  * Return: On success 1.
